2a444588b342e06176e9b6f36818a8e27215439e
shinobigoutcoco <freesweetrainbow@gmail.com>
2023-11-07 14:34:53 +0100

    test ipad navbar

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
   if (window.scrollY < lastScroll) {
-    sidebar.style.top = "30px";
+    sidebar.style.top = "40px";
   } else {
     sidebar.style.top = "-100px";
   }
