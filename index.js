f018b6f33868f55f72fa6416e471633f3966958c
shinobigoutcoco <freesweetrainbow@gmail.com>
2023-11-07 15:05:16 +0100

    test ipad 3

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const sidebar = document.getElementById("side-bar");
 const content = document.querySelector(".content");
+const h1 = document.querySelector(".title-container h1");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
+
 content.addEventListener("click", () => {
   sidebar.classList.remove("active");
 });
@@ -12,11 +14,20 @@ content.addEventListener("click", () => {
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY < lastScroll) {
-    sidebar.style.top = "40px";
+  if (isScrolledToTop()) {
+    sidebar.style.top = "30";
   } else {
-    sidebar.style.top = "-100px";
+    if (window.scrollY < lastScroll) {
+      sidebar.style.top = "40px";
+    } else {
+      sidebar.style.top = "-100px";
+    }
   }
 
   lastScroll = window.scrollY;
 });
+
+function isScrolledToTop() {
+  const rect = h1.getBoundingClientRect();
+  return rect.top >= 30;
+}
