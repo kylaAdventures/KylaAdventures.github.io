b7045222ef869acea8bdcd1131dba39e6c1c50b9
shinobigoutcoco <freesweetrainbow@gmail.com>
2023-11-07 17:49:53 +0100

    changement navbar

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+// const sidebar = document.getElementById("side-bar");
+// const content = document.querySelector(".content");
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", () => {
+//   sidebar.classList.toggle("active");
+// });
+// content.addEventListener("click", () => {
+//   sidebar.classList.remove("active");
+// });
+
+// let lastScroll = 0;
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY < lastScroll) {
+//     sidebar.style.top = "80px";
+//   } else {
+//     sidebar.style.top = "-100px";
+//   }
+
+//   lastScroll = window.scrollY;
+// });
+
+// const sidebar = document.getElementById("side-bar");
+// const content = document.querySelector(".content");
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", () => {
+//   sidebar.classList.toggle("active");
+// });
+// content.addEventListener("click", () => {
+//   sidebar.classList.remove("active");
+// });
+
+// let lastScroll = 0;
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY < lastScroll) {
+//     sidebar.style.left = "-230px";
+//   } else {
+//     sidebar.style.left = "-500px";
+//   }
+
+//   lastScroll = window.scrollY;
+// });
+
 const sidebar = document.getElementById("side-bar");
 const content = document.querySelector(".content");
 const btn = document.getElementById("btn");
@@ -13,9 +59,11 @@ let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
   if (window.scrollY < lastScroll) {
-    sidebar.style.top = "80px";
+    sidebar.style.opacity = "1";
+    sidebar.style.visibility = "visible";
   } else {
-    sidebar.style.top = "-100px";
+    sidebar.style.opacity = "0";
+    sidebar.style.visibility = "hidden";
   }
 
   lastScroll = window.scrollY;
