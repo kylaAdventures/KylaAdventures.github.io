7de67b07cb236341388b6e30804348c1a2620f10
shinobigoutcoco <freesweetrainbow@gmail.com>
2023-11-09 22:38:07 +0100

    test8

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const navbar = document.getElementById("navbar");
+// const navbar = document.getElementById("navbar");
 
+// let lastScroll = 0;
+// let timeout;
+// window.addEventListener("scroll", () => {
+//   let scrollValue =
+//     (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+//   if (window.scrollY < lastScroll || scrollValue < 0.2) {
+//     clearTimeout(timeout);
+//     navbar.style.opacity = "1";
+//     navbar.style.visibility = "visible";
+//   } else {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//       navbar.style.opacity = "0";
+//       navbar.style.visibility = "hidden";
+//     }, 1000);
+//   }
+
+//   lastScroll = window.scrollY;
+// });
+
+const navbar = document.getElementById("navbar");
 let lastScroll = 0;
 let timeout;
+const viewportHeight = window.innerHeight;
+
 window.addEventListener("scroll", () => {
   let scrollValue =
-    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    (window.scrollY + viewportHeight) / document.body.offsetHeight;
   if (window.scrollY < lastScroll || scrollValue < 0.2) {
     clearTimeout(timeout);
     navbar.style.opacity = "1";
